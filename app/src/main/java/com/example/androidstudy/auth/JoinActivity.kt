1657c82504fc9b04a24922b103ddbc636f181e7c
choinara0 <chlskfk6810@naver.com>
2021-12-30 22:53:01 +0900

    Add login, join activity

---
+++ app/src/main/java/com/example/androidstudy/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidstudy.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidstudy.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
