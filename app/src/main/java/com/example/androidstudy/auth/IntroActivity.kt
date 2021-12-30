b3c99d2ee45b9d5640753bff8370b0c591cb6ff4
choinara0 <chlskfk6810@naver.com>
2021-12-31 01:04:10 +0900

    비회원 로그인 추가

--- app/src/main/java/com/example/androidstudy/auth/IntroActivity.kt
+++ app/src/main/java/com/example/androidstudy/auth/IntroActivity.kt
@@ -3,16 +3,22 @@ package com.example.androidstudy.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.androidstudy.MainActivity
 import com.example.androidstudy.R
 import com.example.androidstudy.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
-
+    private lateinit var auth : FirebaseAuth
     private lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
@@ -24,6 +30,20 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인 성공 시 mainActivity로 이동
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
