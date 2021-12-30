ff94384170454446fcb3bec5f0c8fade3ce92ffa
choinara0 <chlskfk6810@naver.com>
2021-12-31 00:51:57 +0900

    회원가입 성공 시 mainactivity로 이동

--- app/src/main/java/com/example/androidstudy/auth/JoinActivity.kt
+++ app/src/main/java/com/example/androidstudy/auth/JoinActivity.kt
@@ -63,8 +63,9 @@ class JoinActivity : AppCompatActivity() {
                 auth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
-
+                            //회원가입 성공 시 mainActivity로 이동, joinActivity 종료
                             val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                             startActivity(intent)
 
                         } else {
