d3e8e5cb68f943e32e667edfa453e7ea91e189e0
choinara0 <chlskfk6810@naver.com>
2021-12-30 22:06:10 +0900

    Splash activity와 IntroActivity Intent

--- app/src/main/java/com/example/androidstudy/auth/IntroActivity.kt
+++ app/src/main/java/com/example/androidstudy/auth/IntroActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidstudy.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidstudy.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
