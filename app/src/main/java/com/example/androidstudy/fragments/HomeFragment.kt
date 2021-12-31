3837a68f118c4209febc54ddadf4fef9ebe23968
choinara0 <chlskfk6810@naver.com>
2021-12-31 17:50:23 +0900

    각 fragment에서 각 fragment로 이동하는 로직 구현 (store fragment) 제외

--- app/src/main/java/com/example/androidstudy/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/androidstudy/fragments/HomeFragment.kt
@@ -11,16 +11,6 @@ import androidx.navigation.findNavController
 import com.example.androidstudy.R
 import com.example.androidstudy.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
 
