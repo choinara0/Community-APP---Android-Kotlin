df07e59f54ec9ac0574b6e643880b99ddc85f23d
choinara0 <chlskfk6810@naver.com>
2022-01-02 01:16:45 +0900

    recycleview에 itemclick 추가

--- app/src/main/java/com/example/androidstudy/contentsList/ContentRVAdapter.kt
+++ app/src/main/java/com/example/androidstudy/contentsList/ContentRVAdapter.kt
@@ -1,13 +1,22 @@
 package com.example.androidstudy.contentsList
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.androidstudy.R
 
-class ContentRVAdapter(val items: ArrayList<ContentModel>) : RecyclerView.Adapter<ContentRVAdapter.Viewholder>() {
+class ContentRVAdapter(val context : Context, val items: ArrayList<ContentModel>) : RecyclerView.Adapter<ContentRVAdapter.Viewholder>() {
+
+    interface ItemClick {
+        fun onClick(view : View, position: Int)
+    }
+
+    var itemClick : ItemClick? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
@@ -15,6 +24,13 @@ class ContentRVAdapter(val items: ArrayList<ContentModel>) : RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: ContentRVAdapter.Viewholder, position: Int) {
+
+        if(itemClick != null){
+            holder.itemView.setOnClickListener{ v->
+                itemClick?.onClick(v, position)
+            }
+        }
+
         holder.bindItems(items[position])
     }
 
@@ -28,6 +44,11 @@ class ContentRVAdapter(val items: ArrayList<ContentModel>) : RecyclerView.Adapte
 
             val contentTitle = itemView.findViewById<TextView>(R.id.textArea)
             contentTitle.text = item.title
+
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+            Glide.with(context)
+                .load(item.imageUrl)
+                .into(imageViewArea)
 
         }
     }
