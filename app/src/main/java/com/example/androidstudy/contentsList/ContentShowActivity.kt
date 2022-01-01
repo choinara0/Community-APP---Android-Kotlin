227faf881026f5f394da5e17ce8c77eb8370076b
choinara0 <chlskfk6810@naver.com>
2022-01-02 01:29:59 +0900

    webView에 url 넘겨주기

--- app/src/main/java/com/example/androidstudy/contentsList/ContentShowActivity.kt
+++ app/src/main/java/com/example/androidstudy/contentsList/ContentShowActivity.kt
@@ -2,10 +2,19 @@ package com.example.androidstudy.contentsList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Toast
+import com.example.androidstudy.R
 
 class ContentShowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_show)
+
+        val getUrl = intent.getStringExtra("url")
+
+        val webView : WebView = findViewById(R.id.webView)
+        webView.loadUrl(getUrl.toString())
+
     }
 }
