0dc90525a75c98edd5bbd2ea4c6a653a32305661
choinara0 <chlskfk6810@naver.com>
2022-01-02 01:29:03 +0900

    webUrl을 contentshowactivity로 넘겨주기

--- app/src/main/java/com/example/androidstudy/contentsList/ContentListActivity.kt
+++ app/src/main/java/com/example/androidstudy/contentsList/ContentListActivity.kt
@@ -1,7 +1,10 @@
 package com.example.androidstudy.contentsList
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,15 +19,26 @@ class ContentListActivity : AppCompatActivity() {
 
         val items = ArrayList<ContentModel>()
 
-        items.add(ContentModel("imageurl1", "title1"))
-        items.add(ContentModel("imageurl2", "title2"))
-        items.add(ContentModel("imageurl3", "title3"))
+        items.add(ContentModel("title1","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png", "https://philosopher-chan.tistory.com/1235?category=941578"))
+        items.add(ContentModel("title2","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png", "https://philosopher-chan.tistory.com/1236?category=941578"))
+        items.add(ContentModel("title3","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png", "https://philosopher-chan.tistory.com/1237?category=941578"))
 
 
-        val rvAdapter = ContentRVAdapter(items)
+        val rvAdapter = ContentRVAdapter(baseContext, items)
         rv.adapter = rvAdapter
 
         rv.layoutManager = GridLayoutManager(this, 2)
+
+        rvAdapter.itemClick = object : ContentRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                Toast.makeText(baseContext, items[position].title, Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this@ContentListActivity, ContentShowActivity::class.java)
+                intent.putExtra("url", items[position].webUrl)
+                startActivity(intent)
+            }
+
+        }
 
 
     }
