af939b1197ce52f574581f7a305dfb7652885a48
choinara0 <chlskfk6810@naver.com>
2022-01-06 21:54:45 +0900

    bookmark된 정보와 전체 컨텐츠를 비교하여 북마크 정보만 보여주기

--- app/src/main/java/com/example/androidstudy/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/example/androidstudy/fragments/BookmarkFragment.kt
@@ -1,19 +1,35 @@
 package com.example.androidstudy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidstudy.R
+import com.example.androidstudy.contentsList.BookmarkRVAdapter
+import com.example.androidstudy.contentsList.ContentModel
 import com.example.androidstudy.databinding.FragmentBookmarkBinding
+import com.example.androidstudy.utils.FBAuth
+import com.example.androidstudy.utils.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 
 class BookmarkFragment : Fragment() {
 
     private lateinit var binding : FragmentBookmarkBinding
+    private val TAG = BookmarkFragment::class.java.simpleName
+    lateinit var rvAdapter : BookmarkRVAdapter
+
+    val bookmarkIdList = mutableListOf<String>()
+    val items = ArrayList<ContentModel>()
+    val itemKeyList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +42,18 @@ class BookmarkFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
+
+        // 2. 사용자가 북마크한 정보를 다 가져오기
+        getBookmarkData()
+
+        // 3. 전체 컨텐츠 중에서, 사용자가 북마크한 정보만 보여주기
+
+        rvAdapter = BookmarkRVAdapter(requireContext(), items, itemKeyList, bookmarkIdList)
+
+        val rv : RecyclerView = binding.bookmarkRV
+        rv.adapter = rvAdapter
+        rv.layoutManager = GridLayoutManager(requireContext(), 2)
+
 
         binding.homeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_bookmarkFragment_to_homeFragment)
@@ -44,6 +72,62 @@ class BookmarkFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+
+    private fun getCategoryData(){
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+                    Log.d(TAG + 1, dataModel.toString())
+                    val item = dataModel.getValue(ContentModel::class.java)
+
+                    // 3. 전체 컨텐츠 중에서, 사용자가 북마크한 정보만 보여줌!
+                    if (bookmarkIdList.contains(dataModel.key.toString())){
+                        items.add(item!!)
+                        itemKeyList.add(dataModel.key.toString())
+                    }
+
+
+                }
+                rvAdapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("ContentListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.category1.addValueEventListener(postListener)
+        FBRef.category2.addValueEventListener(postListener)
+
+    }
+
+    private fun getBookmarkData(){
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+                    Log.e(TAG, dataModel.toString())
+                    bookmarkIdList.add(dataModel.key.toString())
+
+                }
+
+                // 1. 전체 카테고리에 있는 컨텐츠 데이터들을 다 가져옴!
+                getCategoryData()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("ContentListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
+
     }
 
 }
