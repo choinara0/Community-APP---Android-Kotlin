3f3df4bde9634534b2c12514eb263b083b6e9125
choinara0 <chlskfk6810@naver.com>
2022-01-06 21:54:16 +0900

    glide추가해서 imageUrl 넣기

--- app/src/main/java/com/example/androidstudy/contentsList/BookmarkRVAdapter.kt
+++ app/src/main/java/com/example/androidstudy/contentsList/BookmarkRVAdapter.kt
@@ -2,11 +2,13 @@ package com.example.androidstudy.contentsList
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.androidstudy.R
@@ -14,20 +16,21 @@ import com.example.androidstudy.utils.FBAuth
 import com.example.androidstudy.utils.FBRef
 
 class BookmarkRVAdapter(val context : Context,
-                       val items: ArrayList<ContentModel>,
-                       val keyList : ArrayList<String>,
-                       val bookmarkIdList : MutableList<String>)
-    : RecyclerView.Adapter<BookmarkRVAdapter.Viewholder>() {
+                        val items : ArrayList<ContentModel>,
+                        val keyList : ArrayList<String>,
+                        val bookmarkIdList : MutableList<String>)
 
+    : RecyclerView.Adapter<BookmarkRVAdapter.Viewholder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookmarkRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
+
+        Log.d("BookmarkRVAdapter", keyList.toString())
+        Log.d("BookmarkRVAdapter", bookmarkIdList.toString())
         return Viewholder(v)
     }
 
     override fun onBindViewHolder(holder: BookmarkRVAdapter.Viewholder, position: Int) {
-
-
         holder.bindItems(items[position], keyList[position])
     }
 
@@ -35,44 +38,36 @@ class BookmarkRVAdapter(val context : Context,
         return items.size
     }
 
-    inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindItems(item : ContentModel, key : String) {
 
-            itemView.setOnClickListener{
+            itemView.setOnClickListener {
+                Toast.makeText(context, item.title, Toast.LENGTH_LONG).show()
                 val intent = Intent(context, ContentShowActivity::class.java)
                 intent.putExtra("url", item.webUrl)
                 itemView.context.startActivity(intent)
             }
 
             val contentTitle = itemView.findViewById<TextView>(R.id.textArea)
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+            val bookmarkArea = itemView.findViewById<ImageView>(R.id.bookmarkArea)
+
+            if(bookmarkIdList.contains(key)) {
+                bookmarkArea.setImageResource(R.drawable.bookmark_color)
+            } else {
+                bookmarkArea.setImageResource(R.drawable.bookmark_white)
+            }
+
             contentTitle.text = item.title
 
-            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
             Glide.with(context)
                 .load(item.imageUrl)
                 .into(imageViewArea)
 
-            val bookmarkArea = itemView.findViewById<ImageView>(R.id.bookmarkArea)
-
-            if(bookmarkIdList.contains(key)){
-                bookmarkArea.setImageResource(R.drawable.bookmark_color)
-            }else {
-                bookmarkArea.setImageResource(R.drawable.bookmark_white)
-            }
-
-            bookmarkArea.setOnClickListener {
-                //bookmark가 있을 때 삭제
-                if(bookmarkIdList.contains(key)){
-                    FBRef.bookmarkRef.child(FBAuth.getUid()).child(key).removeValue()
-                    //bookmark가 없을 때 추가
-                } else{
-                    FBRef.bookmarkRef.child(FBAuth.getUid()).child(key).setValue(BookmarkModel(true))
-                }
-
-            }
-
         }
+
     }
+
 
 }
