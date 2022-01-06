be16f8ab309b5165a3d438ac867a5ad4589669b0
choinara0 <chlskfk6810@naver.com>
2022-01-06 21:53:41 +0900

    오타 수정

--- app/src/main/java/com/example/androidstudy/utils/FBRef.kt
+++ app/src/main/java/com/example/androidstudy/utils/FBRef.kt
@@ -8,8 +8,11 @@ class FBRef {
 
     companion object{
 
-        private lateinit var myRef : DatabaseReference
         private val database = Firebase.database
+
+        val category1 = database.getReference("contents")
+        val category2 = database.getReference("contents2")
+
         val bookmarkRef = database.getReference("bookmark_list")
 
     }
