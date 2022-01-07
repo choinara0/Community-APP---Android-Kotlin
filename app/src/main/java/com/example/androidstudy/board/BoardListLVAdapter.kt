f697aec6d1fbe5bc36f056ddab3bf07ba3cffdf0
choinara0 <chlskfk6810@naver.com>
2022-01-08 00:09:04 +0900

    각 Area값을 받아서 title, content, time에 넣어주기

--- app/src/main/java/com/example/androidstudy/board/BoardListLVAdapter.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardListLVAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import com.example.androidstudy.R
 
 class BoardListLVAdapter(val boardList : MutableList<BoardModel>) : BaseAdapter() {
@@ -28,6 +29,15 @@ class BoardListLVAdapter(val boardList : MutableList<BoardModel>) : BaseAdapter(
             view = LayoutInflater.from(parent?.context).inflate(R.layout.board_list_item, parent, false)
 
         }
+
+        val title = view?.findViewById<TextView>(R.id.titleArea)
+        title!!.text = boardList[position].title
+
+        val content = view?.findViewById<TextView>(R.id.contentArea)
+        content!!.text = boardList[position].content
+
+        val time = view?.findViewById<TextView>(R.id.timeArea)
+        time!!.text = boardList[position].time
 
         return view!!
 
