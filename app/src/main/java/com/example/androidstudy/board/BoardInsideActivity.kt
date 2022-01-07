a384ab1d45402d4ab43e7e4083e329df00bd389e
choinara0 <chlskfk6810@naver.com>
2022-01-08 00:34:14 +0900

    getExtra한 값으로 binding하기

--- app/src/main/java/com/example/androidstudy/board/BoardInsideActivity.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardInsideActivity.kt
@@ -3,19 +3,28 @@ package com.example.androidstudy.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.example.androidstudy.R
+import com.example.androidstudy.databinding.ActivityBoardInsideBinding
 
 class BoardInsideActivity : AppCompatActivity() {
 
     private val TAG = BoardInsideActivity::class.java.simpleName
 
+    private lateinit var binding : ActivityBoardInsideBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_inside)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
         val title = intent.getStringExtra("title").toString()
         val content = intent.getStringExtra("content").toString()
         val time = intent.getStringExtra("time").toString()
+
+        binding.titleArea.text = title
+        binding.contentArea.text = content
+        binding.timeArea.text = time
 
         Log.d(TAG, title)
         Log.d(TAG, content)
