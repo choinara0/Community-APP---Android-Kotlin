1590b54d2a4fba11cd633182620635e8a058c35c
choinara0 <chlskfk6810@naver.com>
2022-01-07 22:30:00 +0900

    writeBtn 클릭 시 FB board에 title,content,uid,time 추가

--- app/src/main/java/com/example/androidstudy/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardWriteActivity.kt
@@ -2,10 +2,41 @@ package com.example.androidstudy.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.androidstudy.R
+import com.example.androidstudy.databinding.ActivityBoardWriteBinding
+import com.example.androidstudy.utils.FBAuth
+import com.example.androidstudy.utils.FBRef
+import com.example.androidstudy.utils.Time
 
 class BoardWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityBoardWriteBinding
+
+    private val TAG = BoardWriteActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_write)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
+
+        binding.writeBtn.setOnClickListener {
+
+            var title = binding.titleArea.text.toString()
+            var content = binding.contentArea.text.toString()
+            val uid = FBAuth.getUid()
+            val time = Time.getTime()
+
+            Log.d(TAG, title)
+            Log.d(TAG, content)
+
+            FBRef.boardRef
+                .push()
+                .setValue(BoardModel(title, content, uid, time))
+
+
+        }
+
     }
 }
