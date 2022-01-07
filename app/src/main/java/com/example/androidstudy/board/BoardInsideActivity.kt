9d9071e869857ae0894b7adff5e27f9793f649a7
choinara0 <chlskfk6810@naver.com>
2022-01-08 00:25:10 +0900

    게시글 클릭시 넘긴 title, content, time 받아오기

--- app/src/main/java/com/example/androidstudy/board/BoardInsideActivity.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardInsideActivity.kt
@@ -2,10 +2,24 @@ package com.example.androidstudy.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.androidstudy.R
 
 class BoardInsideActivity : AppCompatActivity() {
+
+    private val TAG = BoardInsideActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_inside)
+
+        val title = intent.getStringExtra("title").toString()
+        val content = intent.getStringExtra("content").toString()
+        val time = intent.getStringExtra("time").toString()
+
+        Log.d(TAG, title)
+        Log.d(TAG, content)
+        Log.d(TAG, time)
+
     }
 }
