24d42d9481c5b3bb8e44ad555b119d95e8f3869a
choinara0 <chlskfk6810@naver.com>
2022-01-07 22:20:37 +0900

    BoardModel 설정

--- app/src/main/java/com/example/androidstudy/board/BoardModel.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardModel.kt
@@ -1,4 +1,8 @@
 package com.example.androidstudy.board
 
-class BoardModel {
-}
+data class BoardModel(
+    val title : String?,
+    val content : String?,
+    val uid : String?,
+    val time : String
+)
