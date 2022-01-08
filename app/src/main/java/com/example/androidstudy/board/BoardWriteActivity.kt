595245788a656ddd2cb56d36563459eda54848ad
choinara0 <chlskfk6810@naver.com>
2022-01-08 14:30:24 +0900

    디바이스에 있는 이미지 받아와서 imageArea에 넣기

--- app/src/main/java/com/example/androidstudy/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardWriteActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidstudy.board
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -42,5 +44,17 @@ class BoardWriteActivity : AppCompatActivity() {
 
         }
 
+        binding.imageArea.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 100){
+            binding.imageArea.setImageURI(data?.data)
+        }
     }
 }
