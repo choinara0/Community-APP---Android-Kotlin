8fdba6fdacfa22a05c553a44223193cf09bc04a6
choinara0 <chlskfk6810@naver.com>
2022-01-10 23:57:44 +0900

    수정 버튼 클릭 시 boardEditActivty에 수정 전 내용 보여주기

--- app/src/main/java/com/example/androidstudy/board/BoardEditActivity.kt
+++ app/src/main/java/com/example/androidstudy/board/BoardEditActivity.kt
@@ -2,10 +2,73 @@ package com.example.androidstudy.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.example.androidstudy.R
+import com.example.androidstudy.databinding.ActivityBoardEditBinding
+import com.example.androidstudy.utils.FBRef
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.lang.Exception
 
 class BoardEditActivity : AppCompatActivity() {
+
+    private lateinit var key : String
+    private lateinit var binding : ActivityBoardEditBinding
+    private val TAG = BoardEditActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_edit)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_edit)
+
+        key = intent.getStringExtra("key").toString()
+
+        getBoardData(key)
+        getImageData(key)
+    }
+
+    private fun getBoardData(key: String) {
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                try{
+                    val dataModel = dataSnapshot.getValue(BoardModel::class.java)
+                    binding.titleArea.setText(dataModel!!.title)
+                    binding.contentArea.setText(dataModel!!.content)
+
+                }catch (e : Exception){
+                    Log.d(TAG, "삭제완료 ")
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.child(key).addValueEventListener(postListener)
+
+    }
+
+    private fun getImageData(key: String) {
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+
+        // ImageView in your Activity
+        val imageViewFromFB = binding.imageArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if(task.isSuccessful){
+                Glide.with(this)
+                    .load(task.result).into(imageViewFromFB)
+            }
+        })
     }
 }
