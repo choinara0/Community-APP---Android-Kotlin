91224bebe7c8ce3c805c3e9af193274a88f00c87
choinara0 <chlskfk6810@naver.com>
2022-01-11 22:14:49 +0900

    settingBtn 클릭 시 settingActivity로 이동

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import com.example.androidstudy.auth.IntroActivity
+import com.example.androidstudy.setting.SettingActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         auth = Firebase.auth
 
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.settingBtn).setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
 
 //        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
 //            auth.signOut()
